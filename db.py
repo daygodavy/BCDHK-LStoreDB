9e932c19ed0a20b6f19161515744584f225c4f9d
csoth25 <tsoth25@gmail.com>
2020-02-21 16:01:27 -0800

    working on open

--- db.py
+++ db.py
@@ -1,19 +1,31 @@
+import os
+
 from table import Table
 from config import *
-
 
 class Database:
 
     def __init__(self):
         self.tables = {}
-        pass
 
-    def open(self):
-        pass
+    def open(self, directory_name):
+        # path = directory_name + '/tables'
+        # print("new path: ", path)
+        directory_list = os.listdir(directory_name)
+        print("directory list: ", directory_list)
+
+        # for _, name in enumerate(directory_list):
+        #     newpath = path + '/' + name
+        #     self.create_table(newpath)
 
     def close(self):
         pass
 
+    def get_table(self, name):
+        """
+        # Returns table with the passed name
+        """
+        return self.tables(name)
 
     def create_table(self, name, num_columns, key):
         """
@@ -26,7 +38,7 @@ class Database:
         :return: table object       # the table object being created
         """
         table = Table(name, num_columns, key + NUMBER_OF_META_COLUMNS)
-        return table
+        self.tables[table.name] = table
 
     def drop_table(self, name):
         """
@@ -34,5 +46,16 @@ class Database:
 
         :param name: string         # The name of the table
         """
-        pass
-      
+        tobj = self.tables.pop(name, 0)
+        if tobj == 0:
+            print('{} table does not exist'.format(name))
+        else:
+            del tobj
+            print('{} table dropped'.format(name))
+
+
+db = Database()
+db.open('~/ECS165')
+# tbl = db.create_table('Users', 2, 0)
+# db.drop_table('Users')
+# db.get_table('test.txt')
